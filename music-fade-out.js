c07a1f7b602f44c93ef4922eacb858bd9be6305e
Corey Steiner <corey.joseph.steiner@gmail.com>
2024-01-29 21:46:40 -0500

    update

--- music-fade-out.js
+++ music-fade-out.js
@@ -1,14 +1,19 @@
 function fadeOutPlaylist(playlist){
-	if(!playing.playing) return;
+	if(!playlist.playing) return
 	let settingsFadeDuration = game.settings.get('global-playlist-control', 'duration')
 	let playingSound = playlist.sounds.filter(s => s.playing).find(_ => true).sound;
 	let curVol = playingSound.volume
-	if(curVol == 0) return;
+	if(curVol == 0) return
 	playingSound.fade(0, {
 		duration: settingsFadeDuration,
 		from: curVol
 	})
-	return;
+	return
+}
+
+function fadeOutPlaying(){
+	game.playlists.filter(p => p.playing).forEach(p => fadeOutPlaylist(p))
+	ui.notifications.info("Stopping all playlists")
 }
 
 Hooks.once("setup", async () => {
@@ -20,7 +25,7 @@ Hooks.once("setup", async () => {
 		type: Number,
 		default: 5000,
 		onChange: value => { }
-	})
+	}
 
 	game.settings.register('global-playlist-control', 'toggle', {
 		name: `Trigger Fade`,
@@ -29,10 +34,6 @@ Hooks.once("setup", async () => {
 		config: false,
 		type: Boolean,
 		default: true,
-		onChange: value => {
-			game.playlists.filter(p => p.playing.forEach(p => {
-				fadeOutPlaylist(p)
-			}))
-		}
-	})
+		onChange: value => { fadeOutPlaying() }
+	}
 })
